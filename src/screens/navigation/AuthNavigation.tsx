1bc609448e76a262824419c4e18575c786e2aa8f
Jayendra <parmarjayendra8860@gmail.com>
2024-09-01 22:07:21 +0530

    starting routing in app

--- src/screens/navigation/AuthNavigation.tsx
+++ src/screens/navigation/AuthNavigation.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AuthNavigation = () => {
   return (
-    <View>
+    <View className='flex-1 items-center justify-center bg-blue-500'> 
       <Text>AuthNavigation</Text>
     </View>
   )
--- src/screens/navigation/RootNavigation.tsx
+++ src/screens/navigation/RootNavigation.tsx
@@ -1,13 +1,25 @@
 import { View, Text } from "react-native";
 import React, { useState } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigation from "./AppNavigation";
+import AuthNavigation from "./AuthNavigation";
+import TabNavigation from "./TabNavigation";
 
 const RootNavigation = () => {
-  const [session, setSession] = useState<boolean>(true);
+  const Stack = createStackNavigator();
+  const [session, setSession] = useState<boolean>(false);
 
   return (
-    <View>
-      <Text>RootNavigation</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {session ? (
+          <Stack.Screen name="TabNavigation" component={TabNavigation} />
+        ) : (
+          <Stack.Screen name="AuthNavigation" component={AuthNavigation} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
